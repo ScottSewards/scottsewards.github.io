09794df31d88c65b9e77df34a330a464cbfa5442
ScottSewards <scotttsewards@hotmail.com>
2020-09-28 23:19:02 +0100

    Improved Velleity formatting
    Updated various style properties to improve overall look.

--- velleity.js
+++ velleity.js
@@ -1,19 +1,14 @@
-window.onload = function() {
-  //$("a[href*='" + location.pathname.substring(8, -1) + "']").addClass("active"); //SET RIBBON
+$().ready(function() {
+  var darkMode = false;
+  if(darkMode) {
 
-  $("[name='show-password']").click(function() { //SHOW PASSWORD
+  } else {
+
+  }
+
+  $("[name='show-password']").click(function() {
     if($(this).val() == "Show") $(this).val("Hide").prev().attr("type", "text");
     else $(this).val("Show").prev().attr("type", "password");
-  });
-
-  $('.slideshow').each(function() { //SLIDESHOW
-    setInterval(function() {
-      var clone = $(this).find("> figure > figcaption").remove();
-      //var source = $(this).find('img:first-child').attr('src');
-      //var alt = $(this).find('img:first-child').attr('alt');
-      //$(this).find("> figure").append("<img src='" + source + "' alt='" + alt + "'></img>").find('img:first-child').remove();
-      //$(this).find("> figure").prepend(clone);
-    }, 5000);
   });
 
   $(".accordion").each(function() {
@@ -51,7 +46,9 @@ window.onload = function() {
       });
     });
   });
-}
+});
+
+//$("a[href*='" + location.pathname.substring(8, -1) + "']").addClass("active"); //SET RIBBON
 
 /*
 $('main a').mouseenter(function(){ //ANCHOR PREVIEW
@@ -63,35 +60,37 @@ $('main a').mouseleave(function(){
   $('div.preview').remove();
 });
 
+$('nav li', 'button', 'a', 'audio', 'img', 'video').each(function() { //ACCESSIBILITY
+  $(this).href('wai-aria', num)
+  num += 1;
+});
 
-//CACHE API
-//INDEXED DB
-//STORAGE MANAGE API
+$('.slideshow').each(function() {
+  setInterval(function() {
+    //var clone = $(this).find("> figure > figcaption").remove();
+    //var source = $(this).find('img:first-child').attr('src');
+    //var alt = $(this).find('img:first-child').attr('alt');
+    //$(this).find("> figure").append("<img src='" + source + "' alt='" + alt + "'></img>").find('img:first-child').remove();
+    //$(this).find("> figure").prepend(clone);
+  }, 5000);
+});
 
-
-  $('nav li', 'button', 'a', 'audio', 'img', 'video').each(function() { //ACCESSIBILITY
-    $(this).href('wai-aria', num)
-    num += 1;
+$(function(){
+  $('#getHere').get("http://teaching.ehustudent.co.uk/add-my-comment/22918078", {/*JSLN},function(){
+    alert(data +  status);
   });
+});
 
+var xhr = new XMLHttpRequest();
 
-  $(function(){
-    var uni = "http://teaching.ehustudent.co.uk/add-my-comment/22918078";
-    $('#getHere').get(uni, {/*JSLN},function(){
-      alert(data +  status);
-    });
-  });
-
-  var xhr = new XMLHttpRequest();
-
-  $(function(){
-    var formData = $('#login').serialize;
-    $.post("test.php", "user=scott", proData).error(proError);
-    //$.postJSON(url, formData{
-    //  "first": "value",
-    //  "second": "value"
-    //}, proData).error(proError);
-  });
+$(function(){
+  var formData = $('#login').serialize;
+  $.post("test.php", "user=scott", proData).error(proError);
+  //$.postJSON(url, formData{
+  //  "first": "value",
+  //  "second": "value"
+  //}, proData).error(proError);
+});
 
   function proData(data, status){
     alert(status);
@@ -100,35 +99,6 @@ $('main a').mouseleave(function(){
 
   function proError(data){
     var msg = $(this).text("There was an error.");
-  }
-
-
-var node = {
-  id: null,
-  forename: "",
-  middle: "",
-  surname: "",
-  maiden: "",
-  sex: null,
-  born: {
-
-  },
-  bornCity: null,
-  died: {
-
-  },
-  diedCity: null,
-  fatherFather: null, //GET FROM FATHER
-  fatherMother: null,
-  father: null,
-  motherFather: null, //GET FROM MOTHER
-  motherMother: null,
-  mother: null,
-  siblings: {
-
-  }, //GET FROM FATHER, THEN MOTHER
-  children: {
-
   }
 }
 */
