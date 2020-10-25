a0f888325f8b2a94a5b62e20fc1e7822af1f2341
ScottSewards <scotttsewards@hotmail.com>
2020-10-25 12:00:06 +0000

    Grid Layout Update
    Renamed albers.html framework.html
    Merged compontent CSS into components.scss
    Updated form style shadow
    Centred p element
    Added code element tab-size
    Removed layout.scss size limits
    Disabled prefers-color-scheme to fix
    Updated index.html content
    Removed albers.js unused jQuery
    Removed jquery.js and some web components

--- js/albers.js
+++ js/albers.js
@@ -1,4 +1,3 @@
-
 //https://developers.google.com/web/fundamentals/web-components/shadowdom
 function createQR(data) {
   var ethereumWallet = "0xA14Ae9BC94005A93934a027024EB7421215853Af";
@@ -33,59 +32,6 @@ function createTable(data, parent) {
     }
   }
 }
-
-
-
-/*
-$(document).ready(function() {
-  //$("a[href*='" + location.pathname.substring(8, -1) + "']").addClass("active"); //SET RIBBON
-
-  $("[name='show-password']").click(function() {
-    if($(this).val() == "Show") $(this).val("Hide").prev().attr("type", "text");
-    else $(this).val("Show").prev().attr("type", "password");
-  });
-
-  $(".accordion").each(function(index) {
-    var accordion = $(this);
-    $(this).find("> p").each(function(index) {
-      $(this).addClass("accordion-" + index);
-    });
-
-    $(this).find("> div").each(function(index) {
-      $(this).addClass("accordion-" + index);
-    });
-
-    $(accordion).find("> p").click(function() {
-      var listItemClasses = $(this).attr('class');
-      $(accordion).find('> p, > div').removeClass('active').each(function() {
-        if($(this).hasClass(listItemClasses)) $(this).addClass('active');
-      });
-    });
-  });
-
-  $('.tabs').each(function(index) {
-    var tabs = $(this);
-    $(this).find('> ol, > ul').addClass('tab-options').find('> li').each(function(index) {
-      $(this).addClass('tab-' + index);
-    });
-
-    $(tabs).find('> div').each(function(index) {
-      $(this).addClass('tab-' + index);
-    });
-
-    $(tabs).find('> ol > li, > ul > li').click(function() {
-      var listItemClasses = $(this).attr('class');
-      $(tabs).find('> ol > li, > ul > li, > div').removeClass('active').each(function() {
-        if($(this).hasClass(listItemClasses)) $(this).addClass('active');
-      });
-    });
-  });
-});
-*/
-
-
-
-
 
 //https://web.dev/file-system-access/
 //https://developer.mozilla.org/en-US/docs/Web/API/FileSystem
--- js/components/anchor-extend.js
+++
@@ -1,6 +0,0 @@
-//https://blog.revillweb.com/extending-native-dom-elements-with-web-components-233350c8e86a
-class AnchorExtend extends HTMLAnchorElement {
-
-}
-
-//customElements.define('');
