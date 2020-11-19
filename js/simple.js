b8ab82da4c4be72befd546241bb1ce11d049815f
ScottSewards <scott.sewards@outlook.com>
2020-11-19 22:51:09 +0000

    Minor Content Update

--- js/simple.js
+++ js/simple.js
@@ -1,4 +1,5 @@
 const $ = document.querySelector.bind(document);
+const $$ = (css, parent = document) => Array.from(parent.querySelectorAll(css));
 
 var themeColourHue = getCookie('themeColourHue');
 if(themeColourHue !== null) document.documentElement.style.setProperty('--col-theme-hue', themeColourHue);
@@ -17,4 +18,34 @@ function getCookie(name) {
 function setCookie(name, value, log) {
   document.cookie = name + '=' + value;
   if(log) console.log(name + ' cookie was set.');
+}
+
+
+window.onload = function() {
+  var slideshowTimer = 1000;
+  var articles = $$(".simple-grid-article");
+  articles[0].classList.add("simple-grid-article-s");
+  articles[articles.length - 2].classList.add("simple-grid-article-m-2");
+  articles[articles.length - 1].classList.add("simple-grid-article-m-3");
+  articles[0].classList.add("simple-grid-article-m-1");
+
+  var index = 0;
+  var jIndex = articles.length - 2;
+  var cPasIndex = 2;
+  var cFutIndex = 2;
+
+  setInterval(function() {
+    articles[index].classList.remove("simple-grid-article-s");
+    articles[jIndex].classList.remove("simple-grid-article-m-" + cPasIndex);
+    jIndex++;
+    if(jIndex == articles.length) jIndex = 0;
+    cPasIndex++;
+    if(cPasIndex > 3) cPasIndex = 1;
+    index++;
+    if(index == articles.length) index = 0;
+    articles[index].classList.add("simple-grid-article-s");
+    articles[index].classList.add("simple-grid-article-m-" + cFutIndex);
+    cFutIndex++;
+    if(cFutIndex > 3) cFutIndex = 1;
+  }, slideshowTimer);
 }
