df8563d52c029a6cd3505c21a6fa6429d2c94f86
ScottSewards <scott.sewards@outlook.com>
2020-11-26 17:50:35 +0000

    Minor Updated
    > Added 5 panel slideshow.

--- js/simple.js
+++ js/simple.js
@@ -20,7 +20,6 @@ function setCookie(name, value, log) {
   if(log) console.log(name + ' cookie was set.');
 }
 
-
 window.onload = function() {
   var slideshowTimer = 3000;
   var articles = $$(".simple-grid-article");
@@ -28,24 +27,45 @@ window.onload = function() {
   articles[articles.length - 2].classList.add("simple-grid-article-m-2");
   articles[articles.length - 1].classList.add("simple-grid-article-m-3");
   articles[0].classList.add("simple-grid-article-m-1");
-
+  articles[articles.length - 4].classList.add("simple-grid-article-l-2");
+  articles[articles.length - 3].classList.add("simple-grid-article-l-3");
+  articles[articles.length - 2].classList.add("simple-grid-article-l-4");
+  articles[articles.length - 1].classList.add("simple-grid-article-l-5");
+  articles[0].classList.add("simple-grid-article-l-1");
   var index = 0;
-  var jIndex = articles.length - 2;
-  var cPasIndex = 2;
-  var cFutIndex = 2;
+  var mIndex = articles.length - 2;
+  var mPasIndex = 2;
+  var mFutIndex = 2;
+  var lIndex = articles.length - 4;
+  var lPasIndex = 2;
+  var lFutIndex = 2;
 
   setInterval(function() {
     articles[index].classList.remove("simple-grid-article-s");
-    articles[jIndex].classList.remove("simple-grid-article-m-" + cPasIndex);
-    jIndex++;
-    if(jIndex == articles.length) jIndex = 0;
-    cPasIndex++;
-    if(cPasIndex > 3) cPasIndex = 1;
+
+    articles[mIndex].classList.remove("simple-grid-article-m-" + mPasIndex);
+    mIndex++;
+    if(mIndex == articles.length) mIndex = 0;
+    mPasIndex++;
+    if(mPasIndex > 3) mPasIndex = 1;
+
+    articles[lIndex].classList.remove("simple-grid-article-l-" + lPasIndex);
+    lIndex++;
+    if(lIndex == articles.length) lIndex = 0;
+    lPasIndex++;
+    if(lPasIndex > 5) lPasIndex = 1;
+
     index++;
     if(index == articles.length) index = 0;
+
     articles[index].classList.add("simple-grid-article-s");
-    articles[index].classList.add("simple-grid-article-m-" + cFutIndex);
-    cFutIndex++;
-    if(cFutIndex > 3) cFutIndex = 1;
+
+    articles[index].classList.add("simple-grid-article-m-" + mFutIndex);
+    mFutIndex++;
+    if(mFutIndex > 3) mFutIndex = 1;
+
+    articles[index].classList.add("simple-grid-article-l-" + lFutIndex);
+    lFutIndex++;
+    if(lFutIndex > 5) lFutIndex = 1;
   }, slideshowTimer);
 }
